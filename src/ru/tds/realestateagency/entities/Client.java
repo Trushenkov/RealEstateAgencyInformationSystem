d76f4403abe3f70baf243382e3d09d15728d962e
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-09 14:13:23 +0300

    Создан класс для сущности "Клиент"

---
+++ src/ru/tds/realestateagency/entities/Client.java
@@ -0,0 +1,77 @@
+package ru.tds.realestateagency.entities;
+
+/**
+ * Класс объекта "Клиент"
+ *
+ * @author Трушенков Дмитрий
+ */
+public class Client {
+
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private int phoneNumber;
+    private String email;
+
+    public Client(String lastName, String firstName, String middleName, int phoneNumber, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Client() {
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
