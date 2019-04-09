f0a3e1d5c4f6708f74386bdb25cfaa3e7e79e748
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-09 13:20:38 +0300

    Изменен размер окна при запуске приложения

--- src/ru/tds/realestateagency/Main.java
+++ src/ru/tds/realestateagency/Main.java
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("views/mainpageScreen.fxml"));
+        primaryStage.setTitle("Информационная система для агентства недвижимости");
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
