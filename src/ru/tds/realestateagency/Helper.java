fd0f9abba4d3f9ad24494840cfac108c156ff65f
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-10 16:43:35 +0300

    Создан вспомогательный класс

---
+++ src/ru/tds/realestateagency/Helper.java
@@ -0,0 +1,38 @@
+package ru.tds.realestateagency;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Класс, содержащий вспомогательные методы
+ *
+ * @author Трушенков Дмитрий
+ */
+public class Helper {
+
+    /**
+     * Метод для перехода на новый экран
+     *
+     * @param pathToFxml путь до файла разметки нового окна
+     */
+    public void changeScreen(String pathToFxml) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(pathToFxml));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root = loader.getRoot();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Информационная система для агентства недвижимости");
+        stage.getScene().getStylesheets().add("ru/tds/realestateagency/css/style.css");
+        stage.showAndWait();
+    }
+}
