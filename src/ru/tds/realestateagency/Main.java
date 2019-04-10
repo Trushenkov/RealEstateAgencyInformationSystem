7d7fcb88528a8924019553e8298b37feea7b3199
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-10 16:45:17 +0300

    Добавлен комментарий к классу

--- src/ru/tds/realestateagency/Main.java
+++ src/ru/tds/realestateagency/Main.java
@@ -6,13 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Класс для запуска приложения
+ *
+ * @author Трушенков Дмитрий
+ */
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/mainpageScreen.fxml"));
         primaryStage.setTitle("Информационная система для агентства недвижимости");
-        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setScene(new Scene(root, 1100, 600));
+        primaryStage.getScene().getStylesheets().add("ru/tds/realestateagency/css/style.css");
         primaryStage.show();
     }
 
