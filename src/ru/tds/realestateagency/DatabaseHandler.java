5fa63d436ddbf44f84b0bab4c8ca3f218bf26c8f
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-14 19:51:34 +0300

    Добавлены строчки для создания соединения с базой

--- src/ru/tds/realestateagency/DatabaseHandler.java
+++ src/ru/tds/realestateagency/DatabaseHandler.java
@@ -17,11 +17,19 @@ public class DatabaseHandler {
      * @author Трушенков Дмитрий
      */
     class Const {
+        //место практики
         static final String DB_HOST = "192.168.3.171";
         static final int DB_PORT = 3306;
         static final String DB_NAME = "realestateagency";
         static final String DB_USER = "test";
         static final String DB_PASSWORD = "w4r=ktp>qrqG";
+
+        //дом
+        static final String DB_HOME_HOST = "localhost";
+        static final int DB_HOME_PORT = 3306;
+        static final String DB_HOME_NAME = "realestateagency";
+        static final String DB_HOME_USER = "root";
+        static final String DB_HOME_PASSWORD = "12345";
     }
 
     private Connection connection;
@@ -32,11 +40,12 @@ public class DatabaseHandler {
      * @return соединение с базой данных
      */
     public Connection createDbConnection() {
-        String connectionUrl = "jdbc:mysql://" + Const.DB_HOST + ":" + Const.DB_PORT + "/" + Const.DB_NAME + "?serverTimezone=UTC";
+        String connectionUrl = "jdbc:mysql://" + Const.DB_HOME_HOST + ":" + Const.DB_HOME_PORT + "/" + Const.DB_HOME_NAME + "?serverTimezone=UTC";
         try {
-            connection = DriverManager.getConnection(connectionUrl, Const.DB_USER, Const.DB_PASSWORD);
+            connection = DriverManager.getConnection(connectionUrl, Const.DB_HOME_USER, Const.DB_HOME_PASSWORD);
         } catch (SQLException e) {
             System.err.println("Ошибка при создании соединения с базой данных.");
+            e.printStackTrace();
         }
         return connection;
     }
