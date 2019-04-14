c005c4bed945a6c5e23d1c1fc9aa05387d8c776c
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-14 21:09:38 +0300

    Добавлены методы для поиска расстояния Левенштейна

--- src/ru/tds/realestateagency/Helper.java
+++ src/ru/tds/realestateagency/Helper.java
@@ -35,4 +35,40 @@ public class Helper {
         stage.getScene().getStylesheets().add("ru/tds/realestateagency/css/style.css");
         stage.show();
     }
+
+    /**
+     * Метод для поиска расстояния Левенштейна
+     *
+     * @param str1 первая строка
+     * @param str2 вторая строка
+     * @return расстояние Левенштейна
+     */
+    public static int levenstain(String str1, String str2) {
+        int[] Di_1 = new int[str2.length() + 1];
+        int[] Di = new int[str2.length() + 1];
+
+        for (int j = 0; j <= str2.length(); j++) {
+            Di[j] = j; // (i == 0)
+        }
+
+        for (int i = 1; i <= str1.length(); i++) {
+            System.arraycopy(Di, 0, Di_1, 0, Di_1.length);
+
+            Di[0] = i; // (j == 0)
+            for (int j = 1; j <= str2.length(); j++) {
+                int cost = (str1.charAt(i - 1) != str2.charAt(j - 1)) ? 1 : 0;
+                Di[j] = min(
+                        Di_1[j] + 1,
+                        Di[j - 1] + 1,
+                        Di_1[j - 1] + cost
+                );
+            }
+        }
+
+        return Di[Di.length - 1];
+    }
+
+    public static int min(int n1, int n2, int n3) {
+        return Math.min(Math.min(n1, n2), n3);
+    }
 }
