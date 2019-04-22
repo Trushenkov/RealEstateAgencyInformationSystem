6a12af6a10c83b5ef5820117e34769c207694e33
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-22 16:11:55 +0300

    Добавлено оповещение об ошибке создания соединения с базой данных

--- src/ru/tds/realestateagency/DatabaseHandler.java
+++ src/ru/tds/realestateagency/DatabaseHandler.java
@@ -1,5 +1,9 @@
 package ru.tds.realestateagency;
 
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -40,12 +44,21 @@ public class DatabaseHandler {
      * @return соединение с базой данных
      */
     public Connection createDbConnection() {
-        String connectionUrl = "jdbc:mysql://" + Const.DB_HOME_HOST + ":" + Const.DB_HOME_PORT + "/" + Const.DB_HOME_NAME + "?serverTimezone=UTC";
+        String connectionUrl = "jdbc:mysql://" + Const.DB_HOST + ":" + Const.DB_PORT+ "/" + Const.DB_NAME+ "?serverTimezone=UTC";
         try {
-            connection = DriverManager.getConnection(connectionUrl, Const.DB_HOME_USER, Const.DB_HOME_PASSWORD);
+            connection = DriverManager.getConnection(connectionUrl, Const.DB_USER, Const.DB_HOME_PASSWORD);
         } catch (SQLException e) {
             //оповещение об ошибке при создании соединения с БД
-            new Helper().changeScreen("/ru/tds/realestateagency/views/alerts/errorCreateDBConnection.fxml");
+//            new Helper().changeScreen("/ru/tds/realestateagency/views/alerts/errorCreateDBConnection.fxml");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Уведомление об ошибке");
+            alert.setHeaderText("Ошибка соединения с базой данных");
+            alert.setContentText("Соединение с базой данных не было установлено. Проверьте настройки подключения к базе.");
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            stage.getIcons().add(new Image(this.getClass().getResource("/ru/tds/realestateagency/images/warning.png").toString()));
+            alert.setResizable(false);
+
+            alert.showAndWait();
         }
 
         return connection;
