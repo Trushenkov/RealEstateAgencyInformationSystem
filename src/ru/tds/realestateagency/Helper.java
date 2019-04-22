4adf250873269a548c73e3ccc510dbe30c802ee3
Трушенков Дмитрий <diman28051999@gmail.com>
2019-04-22 13:22:16 +0300

    Изменен модификатор доступа для метода min(int n1, int n2, int n3)

--- src/ru/tds/realestateagency/Helper.java
+++ src/ru/tds/realestateagency/Helper.java
@@ -49,7 +49,7 @@ public class Helper {
         return Di[Di.length - 1];
     }
 
-    public static int min(int n1, int n2, int n3) {
+    private static int min(int n1, int n2, int n3) {
         return Math.min(Math.min(n1, n2), n3);
     }
 
