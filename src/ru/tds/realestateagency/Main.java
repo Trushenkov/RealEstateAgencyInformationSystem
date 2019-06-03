e0dda5b63a1bab1c6c8019af04187cfcb04591db
Трушенков Дмитрий <diman28051999@gmail.com>
2019-06-03 19:30:06 +0300

    Добалвен запуск приложения в полноэкранном режиме

--- src/ru/tds/realestateagency/Main.java
+++ src/ru/tds/realestateagency/Main.java
@@ -2,9 +2,17 @@ package ru.tds.realestateagency;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 /**
  * Класс для запуска приложения
@@ -13,16 +21,19 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
         primaryStage.setTitle("Информационная система для агентства недвижимости");
         primaryStage.setScene(new Scene(root));
         primaryStage.getScene().getStylesheets().add("ru/tds/realestateagency/css/fullpackstyling.css");
+        primaryStage.setFullScreen(true);
+        primaryStage.setFullScreenExitHint("");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("ALT+F4"));
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
