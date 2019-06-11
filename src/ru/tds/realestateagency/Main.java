e18601dd9709fb69a47f90b4989114244d6f8be1
Трушенков Дмитрий <diman28051999@gmail.com>
2019-06-11 21:55:36 +0300

    Изменен стартовый экран и выполнен рефакторинг

--- src/ru/tds/realestateagency/Main.java
+++ src/ru/tds/realestateagency/Main.java
@@ -14,16 +14,20 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private static final String START_SCREEN_PATH = "views/authorization.fxml";
+    private static final String CSS_FILE_PATH = "ru/tds/realestateagency/css/fullpackstyling.css";
+    private static final String TITLE = "Информационная система для агентства недвижимости";
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
-        primaryStage.setTitle("Информационная система для агентства недвижимости");
+        Parent root = FXMLLoader.load(getClass().getResource(START_SCREEN_PATH));
+        primaryStage.setTitle(TITLE);
         primaryStage.setScene(new Scene(root));
-        primaryStage.getScene().getStylesheets().add("ru/tds/realestateagency/css/fullpackstyling.css");
+        primaryStage.getScene().getStylesheets().add(CSS_FILE_PATH);
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitHint("");
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("ALT+F4"));
